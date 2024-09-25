aa4fb0333c3ac7b29c9fbefb38e6cbae9dec92d7
leesiyeon99 <qsqs9090@naver.com>
2024-09-25 17:20:33 +0900

    FEAT : 플레이어 컨트롤러
    두 명의 플레이어 컨트롤러 만들고 각자 카메라 적용

---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,127 @@
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] Rigidbody2D rigid;
+    [SerializeField] SpriteRenderer render;
+    [SerializeField] Animator animator;
+    [SerializeField] float movePower;
+    [SerializeField] float maxMoveSpeed;
+    [SerializeField] float jumpPower;
+    [SerializeField] float maxFallSpeed;
+
+
+
+    [SerializeField] public int hp = 10;
+    [SerializeField] public int maxHp = 10;
+
+    [SerializeField] bool isGrounded;
+    int curAniHash;
+    float x;
+    private int idleHesh = Animator.StringToHash("Idle");
+    private int runHesh = Animator.StringToHash("Run");
+    private int jumpHesh = Animator.StringToHash("Jump");
+    private int fallHesh = Animator.StringToHash("Fall");
+
+    public int gemCount = 0;
+
+    private void Start()
+    {
+        curAniHash = idleHesh;
+    }
+
+    private void Update()
+    {
+        x = Input.GetAxisRaw("Horizontal");
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Jump();
+        }
+        GroundCheck();
+        //AnimatorPlay();
+    }
+
+    private void FixedUpdate()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        rigid.AddForce(x * movePower * Vector2.right, ForceMode2D.Force);
+        if (rigid.velocity.x > maxMoveSpeed)
+        {
+            rigid.velocity = new Vector2(maxMoveSpeed, rigid.velocity.y);
+        }
+        else if (rigid.velocity.x < -maxMoveSpeed)
+        {
+            rigid.velocity = new Vector2(-maxMoveSpeed, rigid.velocity.y);
+        }
+
+        if (rigid.velocity.y < -maxFallSpeed)
+        {
+            rigid.velocity = new Vector2(rigid.velocity.x, -maxFallSpeed);
+        }
+
+        if (x < 0) render.flipX = true;
+        else if (x > 0) render.flipX = false;
+
+    }
+
+    private void Jump()
+    {
+        if (!isGrounded) return;
+        isGrounded = false;
+        rigid.AddForce(Vector2.up * jumpPower, ForceMode2D.Impulse);
+    }
+
+    private void GroundCheck()
+    {
+        RaycastHit2D hit = Physics2D.Raycast(transform.position + new Vector3(0, -0.5f, 0), Vector2.down, 0.5f);
+        isGrounded = hit.collider != null;
+    }
+
+    private void AnimatorPlay()
+    {
+        int checkAniHash;
+        if (rigid.velocity.y > 0.01f && !isGrounded)
+        {
+            checkAniHash = jumpHesh;
+        }
+        else if (rigid.velocity.y < -0.01f && !isGrounded)
+        {
+            checkAniHash = fallHesh;
+        }
+        else if (rigid.velocity.sqrMagnitude < 0.01f)
+        {
+            checkAniHash = idleHesh;
+        }
+        else
+        {
+            checkAniHash = runHesh;
+        }
+
+        if (curAniHash != checkAniHash)
+        {
+            curAniHash = checkAniHash;
+            animator.Play(curAniHash);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("RedWater"))
+        {
+            Debug.Log("빨간물에 닿았습니다.");
+        }
+        if (collision.gameObject.CompareTag("BlueWater"))
+        {
+            Debug.Log("파란물에 닿았습니다.");
+        }
+    }
+
+
+}
+
